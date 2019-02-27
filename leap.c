3792f895dd6b3dc25f510057a32141926c06d250
karthikeyan1998 <47741331+karthikeyan1998@users.noreply.github.com>
2019-02-27 16:12:22 +0530

    Create leap.c

---
+++ leap.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int year;
+    printf("enter the year:");
+    scanf("%d",&year);
+    if(year%4==0)
+    {
+        if(year%100==0)
+        {
+            if(year%400==0)
+            printf("yes");
+            else
+            printf("no");
+        }
+        else
+        printf("yes");
+    }
+    else
+    printf("no");
+    getch();
+        }
